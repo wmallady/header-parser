00ab49eb5361bdf578f3937d8725ab4fd94bdb24
Fox Mallady <44452561+wmallady@users.noreply.github.com>
2021-11-30 07:35:07 -0600

    Add files via upload

---
+++ parser-header/server.js
@@ -0,0 +1,37 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  var userLang = req.headers["accept-language"]
+  var userIP = req.ip;
+  var userSoft = require("os").userInfo().username;
+
+  res.json({"ipaddress": userIP,"language": userLang, "software": userSoft});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
